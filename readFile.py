4cafe31a55171c4958f61b6c939764806fb15cdb
sisi wang <sisiwang242@gmail.com>
2023-07-18 22:05:16 -0400

    add namesFile & add count for each line

--- readFile.py
+++ readFile.py
@@ -1,12 +1,16 @@
 file = open("inputFile.txt", 'r')
 passfile = open("passFile.txt", 'w')
 failfile = open("failFile.txt", 'w')
+namesfile = open("namesFile.txt", 'w')
+count = 0
 for line in file:
+    count += 1
     splitline = line.split()
     if splitline[2] == "P":
-        passfile.write(line)
+        passfile.write(f"{count}. {line}")
     else:
-        failfile.write(line)
+        failfile.write(f"{count}. {line}")
+    namesfile.write(f"{count}. {splitline[0]}\n")
 
 
 file.close()
